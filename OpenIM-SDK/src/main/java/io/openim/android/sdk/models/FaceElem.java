adff7de0805fc97f76d06ef265077300ae30d8e3
hrxiang <xianghairui@126.com>
2022-03-22 15:15:16 +0800

    Fix bug

--- OpenIM-SDK/src/main/java/io/openim/android/sdk/models/FaceElem.java
+++ OpenIM-SDK/src/main/java/io/openim/android/sdk/models/FaceElem.java
@@ -1,14 +1,14 @@
 package io.openim.android.sdk.models;
 
 public class FaceElem {
-    private String index;
+    private int index;
     private String data;
 
-    public String getIndex() {
+    public int getIndex() {
         return index;
     }
 
-    public void setIndex(String index) {
+    public void setIndex(int index) {
         this.index = index;
     }
 
