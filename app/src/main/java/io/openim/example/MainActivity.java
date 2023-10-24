f976ac276df10442be3bbfd8e7d0aabd41ffec64
oliver <WANGJIN_1163@163.com>
2023-10-24 16:38:33 +0800

    optimize

--- app/src/main/java/io/openim/example/MainActivity.java
+++ app/src/main/java/io/openim/example/MainActivity.java
@@ -11,6 +11,5 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 }
