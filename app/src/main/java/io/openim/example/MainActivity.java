2a54c28eeb9aaf9f525c00a4d6ffe69ca4cbd28d
oliver <WANGJIN_1163@163.com>
2023-11-01 20:06:09 +0800

    optimize

--- app/src/main/java/io/openim/example/MainActivity.java
+++ app/src/main/java/io/openim/example/MainActivity.java
@@ -1,32 +1,9 @@
 package io.openim.example;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.List;
-
-import io.openim.android.sdk.OpenIMClient;
-import io.openim.android.sdk.listener.OnBase;
-import io.openim.android.sdk.listener.OnConnListener;
-import io.openim.android.sdk.listener.OnConversationListener;
-import io.openim.android.sdk.listener.OnMsgSendCallback;
-import io.openim.android.sdk.models.AdvancedMessage;
-import io.openim.android.sdk.models.AtUserInfo;
-import io.openim.android.sdk.models.ConversationInfo;
-import io.openim.android.sdk.models.FileElem;
-import io.openim.android.sdk.models.GroupInfo;
-import io.openim.android.sdk.models.KeyValue;
-import io.openim.android.sdk.models.Message;
-import io.openim.android.sdk.models.OfflinePushInfo;
-import io.openim.android.sdk.models.PictureInfo;
-import io.openim.android.sdk.models.RichMessage;
-import io.openim.android.sdk.models.SearchResult;
-import io.openim.android.sdk.models.SoundElem;
-import io.openim.android.sdk.models.TypeKeySetResult;
-import io.openim.android.sdk.models.UserInfo;
-import io.openim.android.sdk.models.UsersOnlineStatus;
 
 
 /**
